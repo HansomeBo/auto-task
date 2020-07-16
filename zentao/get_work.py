8ffbfc63e73332509cd11fff75ed14592f220e8a
HansomenBo <hansomebo@qq.com>
2020-07-16 13:55:40 +0800

    [xubo]修改文件读取

--- zentao/get_work.py
+++ zentao/get_work.py
@@ -7,8 +7,6 @@ import datetime
 import json
 import logging
 import os
-from urllib.request import urlopen
-from zentao.config import Config
 
 from zentao.work import Work
 
@@ -22,8 +20,13 @@ class GetWork:
         if int(week) > 5:
             logging.info("当前日期 ：" + str(now) + "，星期" + week + "，不需要进行禅道填写。")
             return None
-        work_str = urlopen(Config.work_json).read().decode("utf-8")
-        work_list = json.loads(work_str)
+        abs_path = os.path.dirname(os.path.realpath(__file__))
+        work_list_path = abs_path + '/work_map.json'
+        work_list = {}
+        with open(work_list_path, 'r') as json_file:
+            work_list = json.load(json_file)
+        # work_str = urlopen(Config.work_json).read().decode("utf-8")
+        # work_list = json.loads(work_str)
         date = now.strftime('%Y-%m-%d')
         month = now.strftime('%m')
         work = Work(work_list.get(date), date, month)
