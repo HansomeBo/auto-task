f99266d09199b6136b39f89117c4e2cbe360dfec
HansomenBo <hansomebo@qq.com>
2020-07-16 14:08:47 +0800

    [xubo]添加日志输出

--- zentao/write_zentao.py
+++ zentao/write_zentao.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S',
                         filename=log_path,
-                        filemode='w')
+                        filemode='a')
     # 获取任务明细
     work = GetWork.get_work(None)
     if work:
