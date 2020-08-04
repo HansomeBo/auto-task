42503026035d21e71d5ba9a2729b4c3298457440
HansomenBo <hansomebo@qq.com>
2020-08-04 17:09:00 +0800

    [xubo]添加8月初的工作内容

--- zentao/config.py
+++ zentao/config.py
@@ -8,4 +8,4 @@ class Config:
     pwd = '123456abc'
     host = "http://10.253.40.224:8018"
     work_json = "https://raw.githubusercontent.com/HansomeBo/auto-task/master/zentao/work_map.json"
-    task_url = "/zentao/task-recordEstimate-638.html?onlybody=yes"
+    task_url = "/zentao/task-recordEstimate-688.html?onlybody=yes"
